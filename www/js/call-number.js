fe021532b20815879488854655e7ca811a7e77f0
romarin <romariolopezc@gmail.com>
2015-12-07 20:21:25 -0600

    Esconde noticia

--- www/js/call-number.js
+++ www/js/call-number.js
@@ -1,9 +1,7 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    alert('hola');
     $(".phonecall").click(function () {
-        alert('dos');
         callNumber(this);
     });
 }
@@ -16,7 +14,6 @@ function onErrorCall() {
 }
 function callNumber(phone) {
     var number = $(phone).siblings('.phonenumber').val();
-    alert(number);
     var bypassAppChooser = true;
     window.plugins.CallNumber.callNumber(onSuccessCall, onErrorCall, number, bypassAppChooser);
 }
--- www/js/general.js
+++ www/js/general.js
@@ -1,2 +1,10 @@
+document.addEventListener("deviceready", onDeviceReady, false);
 
-
+function onDeviceReady() {
+    $(".hideNewBtn").click(function () {
+        hideNew(this);
+    });
+}
+function hideNew(e) {
+    $(e).parents('.hideNew').remove();
+}
