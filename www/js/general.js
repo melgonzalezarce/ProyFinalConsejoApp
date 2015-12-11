695cfcad59b96878143ea9ef047da4081346abad
MelGonzalezArce <melgonzalezarce@gmail.com>
2015-12-10 21:21:57 -0600

    Correcciones de diseño, acerca de, camara.

--- www/js/general.js
+++ www/js/general.js
@@ -1,5 +1,7 @@
 document.addEventListener("deviceready", onDeviceReady, false);
-//Manejo de Eventos
+var pictureSource;
+var destinationType;
+
 function onDeviceReady() {
     $(".hideNewBtn").click(function () {
         $(this).parents('.hideNew').remove();
@@ -7,14 +9,40 @@ function onDeviceReady() {
     document.addEventListener("backbutton", onBackKeyDown, false);
     document.addEventListener("volumeupbutton", showSideNav, false);
 
+    //Camara
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+
 }
 //Panel
 function showSideNav() {
     $('.button-collapse').sideNav('show');
 }
 
-
 function onBackKeyDown(e) {
     e.preventDefault();
 }
-//Camara
+
+function onPhotoDataSuccess(imageData) {
+    var smallImage = document.getElementById('imagenPerfil');
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+function onFail() {
+    alert('Falla al iniciar la camara ' + message);
+}
+function capturePhoto() {
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+        quality: 50, destinationType: destinationType.DATA_URL
+    });
+    $('#modal1').closeModal();
+}
+function onPhotoURISuccess(imageURI) {
+    var largeImage = document.getElementById('imagenPerfil');
+    largeImage.src = imageURI;
+}
+function getPhoto(source) {
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+        quality: 50, destinationType: destinationType.FILE_URI, sourceType: source
+    });
+    $('#modal1').closeModal();
+}
